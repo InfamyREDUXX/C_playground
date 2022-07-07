c844ad3eac59c9e1eb9c33321cf3c836a31a5fe5
Luis Jimenez <luisjimz94@gmail.com>
2022-07-07 13:54:05 -0500

    third approach

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int numeros=193, 586, 15, 77, 24, 762, 3, 1092, 85, 497;
+    int numeros[]={193, 586, 15, 77, 24, 762, 3, 1092, 85, 497};
 
-    if (numeros%2==0){
-        printf("par");
+    int numero = numeros[0];
+    for(int numero=0; numero<9;numero++){
+        if (numero%2==0){
+            printf("par\n");
+        }
+        else {
+            printf("impar\n");
+        }
     }
-    else {
-        printf("impar");
-    }
+
+
 
     return 0;
 }
