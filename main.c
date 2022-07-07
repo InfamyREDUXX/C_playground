cae93a6351130b9fda8307f2d9e7e1d80eb351bc
Luis Jiménez <luisjimz94@gmail.com>
2022-07-07 13:08:10 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+int main() {
+    int numero1=193;
+    int numero2=586;
+    int numero3=15;
+    int numero4=77;
+    int numero5=24;
+    int numero6=762;
+    int numero7=3;
+    int numero8=1092;
+    int numero9=85;
+    int numero10=497;
+
+    if (numero1%2==0){
+        printf("numero 1 es par / ");
+    }
+    else {
+        printf("numero 1 es impar / ");
+    }
+
+    if (numero2%2==0){
+        printf("numero 2 es par / ");
+    }
+    else {
+        printf("numero 2 es impar / ");
+    }
+
+    if (numero3%2==0){
+        printf("numero 3 es par / ");
+    }
+    else {
+        printf("numero 3 es impar / ");
+    }
+
+    if (numero4%2==0){
+        printf("numero 4 es par / ");
+    }
+    else {
+        printf("numero 4 es impar / ");
+    }
+
+    if (numero5%2==0){
+        printf("numero 5 es par / ");
+    }
+    else {
+        printf("numero 5 es impar / ");
+    }
+
+    if (numero6%2==0){
+        printf("numero 6 es par / ");
+    }
+    else {
+        printf("numero 6 es impar / ");
+    }
+
+    if (numero7%2==0){
+        printf("numero 7 es par / ");
+    }
+    else {
+        printf("numero 7 es impar / ");
+    }
+
+    if (numero8%2==0){
+        printf("numero 8 es par / ");
+    }
+    else {
+        printf("numero 8 es impar / ");
+    }
+
+    if (numero9%2==0){
+        printf("numero 9 es par / ");
+    }
+    else {
+        printf("numero 9 es impar / ");
+    }
+
+    if (numero10%2==0){
+        printf("numero 10 es par / ");
+    }
+    else {
+        printf("numero 10 es impar / ");
+    }
+    return 0;
+}
