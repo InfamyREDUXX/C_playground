e92eb16230b53740db07e456127f7d7a6668d87e
Luis Jimenez <luisjimz94@gmail.com>
2022-07-07 19:03:30 -0500

    handling strings

--- check-for-pairs.c
+++ check-for-pairs.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-void validateIfPair(int numero) {
-    if (numero % 2 == 0) {
-        printf("el numero es %d par\n", numero);
-    } else {
-        printf("el numero es %d impar\n", numero);
-    }
-}
-
 int main() {
-    int numeros[] = {193, 586, 15, 77, 24, 762, 3, 1092, 85, 497};
 
-    printf("%X\n", &numeros[0]);
-    printf("%X\n", &numeros[0]+1);
-
-    for (int i = 0; i <= 9; i++) {
-        int numero = numeros[i];
-        printf("%d : %X\n",i, &numeros[i]);
+    int wordSize = 4;
+    char word[wordSize];
+    gets(word);
+    for (int i = 0; i < wordSize; i++) {
+        printf("%c", word[i]);
     }
+    for (int i = 0; i < wordSize; i++) {
+        printf("\n%d", word[i]);
+    }
+
     return 0;
 }
